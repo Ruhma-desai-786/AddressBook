5f1bb991d25aee783d8f6a78a3b8499e2271403a
Ruhma-desai-786 <romakesari2734@gmail.com>
2023-06-01 23:17:24 +0530

    address

---
+++ AddressBook.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookProblem
+{
+    public class AddressBook
+    {
+        List<Contact> addressbook = new List<Contact>();
+       
+        public void CreateContact()
+        {
+
+            Contact contact = new Contact();
+            Console.WriteLine("Enter Fisrt Name");
+            contact.Firstname = Console.ReadLine();
+            Console.WriteLine("Enter Last Name");
+            contact.Lastname = Console.ReadLine();
+            Console.WriteLine("Enter Address Name");
+            contact.Address = Console.ReadLine();
+            Console.WriteLine("Enter City Name");
+            contact.City = Console.ReadLine();
+            Console.WriteLine("Enter State Name");
+            contact.State = Console.ReadLine();
+            Console.WriteLine("Enter Zip Code");
+            contact.Zip = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Phone Number");
+            contact.PhoneNumber = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine("Enter e mail");
+            contact.email = Console.ReadLine();
+            addressbook.Add(contact);
+
+        }
+        public void AddContact()
+        {
+            Contact newcontact=new Contact();
+            Console.WriteLine("Enter the FirstName");
+            newcontact.Firstname = Console.ReadLine();
+            foreach(Contact contact in addressbook)
+            {
+                if(contact.Firstname==newcontact.Firstname)
+                {
+                    Console.WriteLine("Person with this Name Already Exists");
+                    return;
+                }
+            }
+           
+            Console.WriteLine("Enter Last Name");
+            newcontact.Lastname = Console.ReadLine();
+            Console.WriteLine("Enter Address Name");
+            newcontact.Address = Console.ReadLine();
+            Console.WriteLine("Enter City Name");
+            newcontact.City = Console.ReadLine();
+            Console.WriteLine("Enter State Name");
+            newcontact.State = Console.ReadLine();
+            Console.WriteLine("Enter Zip Code");
+            newcontact.Zip = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Phone Number");
+            newcontact.PhoneNumber = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine("Enter e mail");
+            newcontact.email = Console.ReadLine();
+            addressbook.Add(newcontact);
+
+        }
+        public void Display()
+        {
+            foreach(Contact contact in addressbook)
+            {
+                Console.WriteLine("FirstName--"+contact.Firstname);
+                Console.WriteLine("LastName--" + contact.Lastname);
+                Console.WriteLine("Address--" + contact.Address);
+                Console.WriteLine("City--" + contact.City);
+                Console.WriteLine("State--" + contact.State);
+                Console.WriteLine("Zip--" + contact.Zip);
+                Console.WriteLine("PhoneNumber--" + contact.PhoneNumber);
+                Console.WriteLine("Email--" + contact.email);
+
+            }
+        }
+       
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace AddressBookProblem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book Program");
+            while(true)
+            {
+                AddressBook adBook = new AddressBook();
+                Console.WriteLine("Enter The Choice \n1)Create Contact\n2)Add Contact");
+                int choice=Convert.ToInt32(Console.ReadLine());
+                switch(choice)
+                {
+                    case 1:
+                        adBook.CreateContact();
+                        adBook.Display();
+                        break;
+                    case 2:
+                        adBook.AddContact();
+                        adBook.Display();
+                        break;
+
+                    default:
+                        Console.WriteLine("Select Proper Choice");
+                        break;
+                }
+               
+            }
+           
+        }
+    }
+}
